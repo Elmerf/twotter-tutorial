7232af5de099bca8ac87d9dae72dc4b5372c9ad6
Elmer Fiqi <61036187+Elmerf@users.noreply.github.com>
2020-12-07 17:31:55 +0700

    Conditional and Loops
    Chapter 4

--- src/components/UserProfile.vue
+++ src/components/UserProfile.vue
@@ -1,10 +1,18 @@
 <template>
   <div class="user-profile">
       <div class="user-profile_user-panel">
-          <h1 class="user-profile_username"> @{{ user.username }}</h1>
-          <div class="user-profile_follower-count">
-            <strong>Followers: </strong> {{ followers }}
-          </div>
+        <h1 class="user-profile_username"> @{{ user.username }}</h1>
+        <div class="user-profile_admin-badge" v-if="user.isAdmin">
+          Admin
+        </div>
+        <div class="user-profile_follower-count">
+          <strong>Followers: </strong> {{ followers }}
+        </div>
+      </div>
+      <div class="user-profile_twoots-wrapper">
+        <div class="user-profile_twoots" v-for="twoot in user.twoots" :key="twoot.id">
+          {{ twoot.content }}
+        </div>
       </div> 
   </div>
 </template>
@@ -22,7 +30,11 @@ export default {
         firstName: 'Elmer Fiqi',
         lastName: 'Tajusaladin',
         email: 'elmerfiqi@gmail.com',
-        isAdmin: true
+        isAdmin: true,
+        twoots: [
+          { id: 1, content: 'Vue 3 is awesome!'},
+          { id: 2, content: 'This Website created By Elmer Fiqi following tutorial from Earth is Square'}
+        ]
       }
     }
   },
@@ -51,23 +63,31 @@ export default {
 
 <style>
 .user-profile {
-    display: grid;
-    grid-template-columns: 1fr 3fr;
-    width: 100%;
-    padding: 50px 5%;
+  display: grid;
+  grid-template-columns: 1fr 3fr;
+  width: 100%;
+  padding: 50px 5%;
 }
 
 .user-profile_user-panel {
-    display: flex;
-    flex-direction: column;
-    margin-right: 50px;
-    padding: 20px;
-    background-color: white;
-    border-radius: 5px;
-    border: 1px solid #DFE3E8;
+  display: flex;
+  flex-direction: column;
+  margin-right: 50px;
+  padding: 20px;
+  background-color: white;
+  border-radius: 5px;
+  border: 1px solid #DFE3E8;
+}
+
+.user-profile_admin-badge {
+  background-color: purple;
+  color: white;
+  border-radius: 5px;
+  margin-right: auto;
+  padding: 0 10px;
 }
 
 h1 {
-    margin: 0;
+  margin: 0;
 }
 </style>
